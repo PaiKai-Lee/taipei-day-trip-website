0c25f57db230255c0ec2d4e689e353ff404da94d
Kai <b790718@gmail.com>
2021-08-06 23:49:58 +0800

    質料庫移至RDS

--- module/db.py
+++ module/db.py
@@ -1,10 +1,12 @@
 from mysql.connector import pooling
 import os
 from dotenv import load_dotenv
-load_dotenv()
+load_dotenv(override=True)
 pool=pooling.MySQLConnectionPool(
-    host="localhost",
+    pool_name="taipeiewb",
+    host=os.getenv('DB_HOST'),
     user=os.getenv('DB_USER'),
+    port=3306,
     password=os.getenv("DB_PASSWORD"),
     database="taipeiweb"
 )
